2b436c9e32a026990d3f2ecf186b273dcda0208a
NoFox <nfox598625@gmail.com>
2023-12-26 11:31:34 -0500

    finished up routes and fixed res.send

--- server.js
+++ server.js
@@ -1,53 +1,44 @@
 const fs = require("fs");
 const path = require("path");
-
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+const allNotes = require("./db/db.json");
 
-const notes = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static("public"));
+app.use(express.json());
 
-app.get("/api/notes", (res, req) => {
-  res.json(allNotes.slice(1));
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    let allNotesData = JSON.parse(data);
+    res.json(allNotesData);
+  });
+});
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  newNote.id = uuidv4();
+
+  allNotes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(allNotes));
+
+  res.json(allNotes);
 });
 
-app.get("/", (res, req) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/notes", (res, req) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-app.get("*", (res, req) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-function createNote(body, notesArray) {
-  const newNote = body;
-  if (!Array.isArray(notesArray)) notesArray = [];
-
-  if (notesArray.length === 0) notesArray.push(0);
-
-  body.id = notesArray[0];
-  notesArray[0]++;
-
-  notesArray.push(newNote);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify(notesArray, null, 2)
-  );
-  return newNote;
-}
-
-app.post("/api/notes", (res, req) => {
-  const newNote = createNote(req.body, allNotes);
-  res.json(newNote);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
